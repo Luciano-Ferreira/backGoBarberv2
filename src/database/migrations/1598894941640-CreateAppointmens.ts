413e35b1ae95cd5b91e61d7fdceb934ecf982796
Luciano Silva <luciano2023silva@gmail.com>
2020-09-02 19:26:46 -0300

    inicializando backend da aplicaçao gobarber

---
+++ src/database/migrations/1598894941640-CreateAppointmens.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAppointmens1598894941640
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
+
+/**
+ * Linha do tempo
+ *
+ * 1st week : Agendamentos
+ * 2st week : Usuários
+ * (new devloper) 3st week : Edição em agendamentos
+ * 4st week : Compras
+ *
+ * Evita que os bancos de dados estejam em outras versões
+ */
---
+++ src/models/Appointment.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  provider: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_: Date;
+
+  @UpdateDateColumn()
+  updated_: Date;
+}
+
+export default Appointment;
